b60e3a11caace95f6658d20266db8b060092dbb3
221B493 <221B493@juet.co.in>
2025-03-27 14:18:51 +0530

    le5

---
+++ LE5/Whiskey.java
@@ -0,0 +1,9 @@
+class Whiskey extends Beverage{
+    protected void addCondiment()
+    {
+        System.out.println("Add some ice");
+    }
+    protected void stir(){
+        System.out.println("stir for 29 sec");
+    }
+}
